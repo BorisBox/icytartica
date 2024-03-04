a593da8ee94ecb851ec9cf4b01aac94fd1f0df7b
unknown <tutorijalisrbijaboris@gmail.com>
2024-03-04 21:28:13 +0100

    Fixing GET error

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    console.log(`Webhook is listening!`);
+app.listen(process.env.PORT,()=>{
+    console.log("webhook is listening");
 });
