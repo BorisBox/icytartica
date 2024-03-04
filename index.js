7a8301169dcd67e5bd1b19ce5d05f9d993ee7030
unknown <tutorijalisrbijaboris@gmail.com>
2024-03-04 21:21:12 +0100

    Changed port

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).send("hello this is webhook setup");
-});
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Webhook is listening!`);
 });
