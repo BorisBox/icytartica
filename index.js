086705eecd065d1ba300f2dc5ba1f1d056443ea6
unknown <tutorijalisrbijaboris@gmail.com>
2024-03-04 22:01:30 +0100

    Adding function to check correct answer

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ app.get('/', (req, res) => {
     res.send('All good :)');
 });
 
+var correct_answers = [];
+var turn = 0;
+
 app.post('/listener', (req, res) => {
     console.log('Received Webhook');
     var parsed = req.body;
     if (parsed.mode == "correct_answers") {
-        console.log(parsed.content.split(""));
+        correct_answers = parsed.content.split("");
+    }
+    if (parsed.mode == "guess") {
+        if (parsed.content == correct_answers[turn]) {
+            res.type('text').send("correct")
+        } else {
+            res.type('text').send("incorrect")
+        }
+    }
+    if (parsed.mode == "turn") {
+        turn++
     }
     res.status(200).send('OK');
 });
